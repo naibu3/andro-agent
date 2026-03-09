ac247f806a1e5da5d4717474d4253d779c4f3ec9
Pablo C <pablipoyo@gmail.com>
2026-03-09 11:23:44 +0100

    Prueba 0.1 - fix

--- agno_install.sh
+++ agno_install.sh
@@ -8,7 +8,3 @@ python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
 pip install -U agno python-dotenv pydantic rich openai
-
-# nano .env
-# OPENROUTER_API_KEY=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
-# OPENROUTER_MODEL=openai/gpt-4o-mini
